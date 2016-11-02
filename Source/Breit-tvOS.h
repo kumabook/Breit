c4cd447e7ec57c4343271b5d3a89f42e5102a97e
Hiroki Kumamoto <kumabook@live.jp>
2016-11-03 08:54:45 +0900

    Support tvOS and watchOS

---
+++ Source/Breit-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Breit-tvOS.h
+//  Breit-tvOS
+//
+//  Created by Hiroki Kumamoto on 11/3/16.
+//  Copyright © 2016 kumabook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Breit-tvOS.
+FOUNDATION_EXPORT double Breit_tvOSVersionNumber;
+
+//! Project version string for Breit-tvOS.
+FOUNDATION_EXPORT const unsigned char Breit_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Breit_tvOS/PublicHeader.h>
+
+
---
+++ Source/Breit-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  Breit-watchOS.h
+//  Breit-watchOS
+//
+//  Created by Hiroki Kumamoto on 11/3/16.
+//  Copyright © 2016 kumabook. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Breit-watchOS.
+FOUNDATION_EXPORT double Breit_watchOSVersionNumber;
+
+//! Project version string for Breit-watchOS.
+FOUNDATION_EXPORT const unsigned char Breit_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Breit_watchOS/PublicHeader.h>
+
+
